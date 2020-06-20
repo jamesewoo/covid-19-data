4410f18f1c8b98b4208938f07012fe21f2c8b736
James Woo <james.w@verypossible.com>
2020-06-20 09:53:19 -0400

    add poetry and black

--- covid_utils.py
+++ covid_utils.py
@@ -6,7 +6,6 @@ import os
 
 
 class Utils:
-
     def __init__(self):
         super().__init__()
         self.counties_csv = r"us-counties.csv"
@@ -15,8 +14,8 @@ class Utils:
 
     def read_counties_csv(self):
         df = pd.read_csv(self.counties_csv, parse_dates=["date"])
-        df["cfr"] = df["deaths"]/df["cases"]
-        df["county_state"] = df["county"]+", "+df["state"]
+        df["cfr"] = df["deaths"] / df["cases"]
+        df["county_state"] = df["county"] + ", " + df["state"]
 
         self.state_names = df["state"].unique()
         return df
@@ -32,33 +31,34 @@ class Utils:
 
     def compute_diffs(self, df, window_size=5, center=True):
 
-        counties = df.pivot(
-            index="date", columns="county_state", values="cases")
+        counties = df.pivot(index="date", columns="county_state", values="cases")
         for county in counties.columns:
-            counties[county+self.diff_1_suffix] = counties[county].diff().rolling(window_size,
-                                                                                  center=center).mean()
-            counties[county+self.diff_2_suffix] = counties[county +
-                                                           self.diff_1_suffix].diff()
+            counties[county + self.diff_1_suffix] = (
+                counties[county].diff().rolling(window_size, center=center).mean()
+            )
+            counties[county + self.diff_2_suffix] = counties[
+                county + self.diff_1_suffix
+            ].diff()
         return counties
 
     def generate_plots(self, counties, state_names):
         for state in state_names:
             county_names = [c for c in counties.columns if c.endswith(state)]
 
-            os.makedirs(f'plots/{state}', exist_ok=True)
+            os.makedirs(f"plots/{state}", exist_ok=True)
 
             for county in county_names:
-                df = counties[[county, county+self.diff_1_suffix, county+self.diff_2_suffix]
-                              ].dropna()
-                df['date'] = df.index.date
+                df = counties[
+                    [county, county + self.diff_1_suffix, county + self.diff_2_suffix]
+                ].dropna()
+                df["date"] = df.index.date
                 if not df.empty:
-                    df.plot.bar(x='date', subplots=True)
-                    plt.savefig(
-                        f'plots/{state}/{county}.png', bbox_inches='tight')
+                    df.plot.bar(x="date", subplots=True)
+                    plt.savefig(f"plots/{state}/{county}.png", bbox_inches="tight")
                     plt.close()
 
     def plot_cases_by_weekday(self, counties, county):
-        daily_increase = counties[[county+self.diff_1_suffix]]
+        daily_increase = counties[[county + self.diff_1_suffix]]
         weekday = daily_increase.groupby([daily_increase.index.weekday]).sum()
         weekday.plot.bar()
 
@@ -69,5 +69,5 @@ if __name__ == "__main__":
     counties = utils.compute_diffs(counties)
 
     # Generate all plots
-    plt.rcParams['figure.figsize'] = [15, 10]
+    plt.rcParams["figure.figsize"] = [15, 10]
     utils.generate_plots(counties, utils.state_names)
